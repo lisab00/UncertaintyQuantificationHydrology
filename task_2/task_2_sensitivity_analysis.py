a561272d07121320b00b7661e43228d75f6574d4
hele-135 <74352271+hele-135@users.noreply.github.com>
2024-11-15 18:34:16 +0100

    change params by fixed value

---
+++ task_2/task_2_sensitivity_analysis.py
@@ -0,0 +1,336 @@
+'''
+Created on 15.11.2024
+
+@author: hfran
+'''
+
+import os
+from pathlib import Path
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from hmg.models import hbv1d012a_py
+from hmg.test import aa_run_model
+from hmg import HBV1D012A
+
+main_dir = Path(r'C:\Users\hfran\Documents\Uni\Master\hydrology\MMUQ_Python_Setup\EclipsePortable426\Data\mmuq_ws2425\hmg\data')
+os.chdir(main_dir)
+
+# Read input text time series as a pandas Dataframe object and
+# cast the index to a datetime object.
+inp_dfe = pd.read_csv(r'time_series___24163005.csv', sep=';', index_col=0)
+inp_dfe.index = pd.to_datetime(inp_dfe.index, format='%Y-%m-%d-%H')
+
+# Read the catchment area in meters squared. The first value is needed
+# only.
+cca_srs = pd.read_csv(r'area___24163005.csv', sep=';', index_col=0)
+ccaa = cca_srs.values[0, 0]
+
+tems = inp_dfe.loc[:, 'tavg__ref'].values  # Temperature.
+ppts = inp_dfe.loc[:, 'pptn__ref'].values  # Preciptiation.
+pets = inp_dfe.loc[:, 'petn__ref'].values  # PET.
+diso = inp_dfe.loc[:, 'diso__ref'].values  # Observed discharge.
+
+tsps = tems.shape[0]  # Number of time steps.
+
+# Conversion constant for mm/hour to m3/s.
+dslr = ccaa / (3600 * 1000)  # For daily res. multiply denominator with 24.
+
+prm_names = [
+        'snw_dth',
+        'snw_ast',
+        'snw_amt',
+        'snw_amf',
+        'snw_pmf',
+
+        'sl0_mse',
+        'sl1_mse',
+
+        'sl0_fcy',
+        'sl0_bt0',
+
+        'sl1_pwp',
+        'sl1_fcy',
+        'sl1_bt0',
+
+        'urr_dth',
+        'lrr_dth',
+
+        'urr_rsr',
+        'urr_tdh',
+        'urr_tdr',
+        'urr_cst',
+        'urr_dro',
+        'urr_ulc',
+
+        'lrr_tdh',
+        'lrr_cst',
+        'lrr_dro',
+
+    ]
+
+bounds_dict = {
+       'snw_dth': (0.00, 0.00),
+        'snw_ast': (-1.0, +1.0),
+        'snw_amt': (-0.0, +2.0),
+        'snw_amf': (0.00, 2.00),
+        'snw_pmf': (0.00, 2.00),
+
+        'sl0_mse': (0.00, 1e+2),
+        'sl1_mse': (0.00, 2e+2),
+
+        'sl0_fcy': (0.00, 2e+2),
+        'sl0_bt0': (0.00, 3.0),
+
+        'sl1_pwp': (0.00, 4e+2),
+        'sl1_fcy': (0.00, 4e+2),
+        'sl1_bt0': (0.00, 4.00),
+
+        'urr_dth': (0.00, 2e+1),
+        'lrr_dth': (0.00, 5.00),
+
+        'urr_rsr': (0.00, 1.00),
+        'urr_tdh': (0.00, 1e+2),
+        'urr_tdr': (0.00, 1.00),
+        'urr_cst': (0.00, 1.00),
+        'urr_dro': (0.00, 1.00),
+        'urr_ulc': (0.00, 1.00),
+
+        'lrr_tdh': (0.00, 1e+4),
+        'lrr_cst': (0.00, 1.00),
+        'lrr_dro': (0.00, 1.00),
+        }
+
+# import optimised parameter vector from Task 1
+iterations_df = pd.read_csv(main_dir / "task_1" / "output_one_per_iteration_tol_0.01_seed_123.csv")
+iterations_df.columns = ["Obj_fct_value", "prm_value"]
+# get best objective function value
+last_objective_function = iterations_df['Obj_fct_value'].iloc[-1]
+print(last_objective_function)
+df = pd.DataFrame(iterations_df['prm_value'])
+
+df['prm_value'] = df['prm_value'].str.replace("[ ", "")
+df['prm_value'] = df['prm_value'].str.replace("[", "")
+df['prm_value'] = df['prm_value'].str.replace("]", "")
+df['prm_value'] = df['prm_value'].str.split()
+df['prm_value'] = [[float(p) for p in prm] for prm in df['prm_value']]
+
+df[prm_names] = pd.DataFrame(df['prm_value'].tolist(), index=df.index)
+# get best parameter vector
+last_prm_values = df.iloc[-1]
+last_prm_values = last_prm_values.drop('prm_value')
+print(last_prm_values)
+
+
+def nse(sim, obs):
+    '''Nash-Suttcliffe Efficiency'''
+    # return 1-nse ne nse to make minimization work
+    return 1 - (1 - (np.sum((obs - sim) ** 2) / np.sum((obs - np.mean(obs)) ** 2)))
+
+
+def objective_function_value(prms, modl_objt, metric: str, diso):
+    # # read out metric string
+    # Dictionary mapping string names to metric functions
+    metrics = {
+        # "sse": sse,
+        # "rmse": rmse,
+        "nse": nse,
+        # "lognse": lognse,
+        # "pbias": pbias
+    }
+
+    # Get the right metric function using dictionary
+    metric_fun = metrics.get(metric)
+
+    if not metric_fun:
+        raise ValueError(f"Function '{metric}' is not recognized.")
+
+    # # simulate model with prms and compute obj value
+    # Pass the current parameters to model object
+    modl_objt.set_parameters(prms)
+
+    # Tell the model object that the simulation is not an optimization.
+    modl_objt.set_optimization_flag(0)
+
+    # Run the model for the given inputs, constants and parameters.
+    modl_objt.run_model()
+
+    # compute obj value with current parms
+    diss = modl_objt.get_discharge()
+    result = metric_fun(diss, diso)
+    return result
+
+
+def plot_output(otps, diss, diso, prm_name, change_value):
+    # Show a figure of the observed vs. simulated river flow.
+    fig = plt.figure()
+
+    plt.plot(inp_dfe.index, diso, label='REF', alpha=0.75)
+    plt.plot(inp_dfe.index, diss, label='SIM', alpha=0.75)
+
+    plt.grid()
+    plt.legend()
+
+    plt.xticks(rotation=45)
+
+    plt.xlabel('Time [hr]')
+    plt.ylabel('Discharge\n[$m^3.s^{-1}$]')
+
+    plt.title('Observed vs. Simulated River Flow')
+
+    # plt.show()
+    fig.savefig(f"C:\\Users\\hfran\\Documents\\Uni\\Master\\hydrology\\MMUQ_Python_Setup\\EclipsePortable426\\Data\\mmuq_ws2425\\hmg\\data\\task_2\\diss_{prm_name}_changed_by_{change_value}.png", bbox_inches='tight')
+
+    plt.close(fig)
+    #===========================================================================
+
+    # Show a figure of some of the internally simulated variables of the model.
+    # This also serves as a diagnostic tool to check whether what is simulated
+    # makes sense or not.
+    fig, axs = plt.subplots(9, 1, figsize=(4, 8), dpi=120, sharex=True)
+
+    (axs_tem,
+     axs_ppt,
+     axs_snw,
+     axs_sl0,
+     axs_sl1,
+     axs_etn,
+     axs_rrr,
+     axs_rnf,
+     axs_bal) = axs
+    #===========================================================================
+
+    # Inputs.
+    axs_tem.plot(inp_dfe['tavg__ref'], alpha=0.85)
+    axs_tem.set_ylabel('TEM\n[°C]')
+
+    axs_ppt.plot(inp_dfe['pptn__ref'], alpha=0.85)
+    axs_ppt.set_ylabel('PPT\n[mm]')
+    #===========================================================================
+
+    # Snow depth.
+    axs_snw.plot(inp_dfe.index, otps[:, otps_lbls['snw_dth']], alpha=0.85)
+    axs_snw.set_ylabel('SNW\n[mm]')
+    #===========================================================================
+
+    # Mositure level in both soil layers.
+    axs_sl0.plot(inp_dfe.index, otps[:, otps_lbls['sl0_mse']], alpha=0.85)
+    axs_sl0.set_ylabel('SL0\n[mm]')
+
+    axs_sl1.plot(inp_dfe.index, otps[:, otps_lbls['sl1_mse']], alpha=0.85)
+    axs_sl1.set_ylabel('SL1\n[mm]')
+    #===========================================================================
+
+    # Potential and simulated evapotranspiration.
+    axs_etn.plot(inp_dfe.index, inp_dfe['petn__ref'], label='PET', alpha=0.85)
+
+    axs_etn.plot(
+        inp_dfe.index, otps[:, otps_lbls['sl1_etn']], label='ETN', alpha=0.85)
+
+    axs_etn.set_ylabel('ETN\n[mm]')
+    axs_etn.legend()
+    #===========================================================================
+
+    # Depth of water in the upper and lower reservoirs.
+    axs_rrr.plot(
+        inp_dfe.index, otps[:, otps_lbls['urr_dth']], label='URR', alpha=0.85)
+
+    axs_rrr.plot(
+        inp_dfe.index, otps[:, otps_lbls['lrr_dth']], label='LRR', alpha=0.85)
+
+    axs_rrr.set_ylabel('DTH\n[mm]')
+    axs_rrr.legend()
+    #===========================================================================
+
+    # Surface and underground runoff.
+    axs_rnf.plot(
+        inp_dfe.index, otps[:, otps_lbls['rnf_sfc']], label='SFC', alpha=0.85)
+
+    axs_rnf.plot(
+        inp_dfe.index, otps[:, otps_lbls['rnf_gnd']], label='GND', alpha=0.85)
+
+    axs_rnf.set_ylabel('RNF\n[mm]')
+    axs_rnf.legend()
+    #===========================================================================
+
+    # Water balance time series at each time step.
+    # Should be close to zero.
+    axs_bal.plot(inp_dfe.index, otps[:, otps_lbls['mod_bal']], alpha=0.85)
+    axs_bal.set_ylabel('BAL\n[mm]')
+    #===========================================================================
+
+    # Some other makeup.
+    for ax in axs: ax.grid()
+
+    axs[-1].set_xlabel('Time [hr]')
+
+    plt.xticks(rotation=45)
+
+    plt.suptitle('Inputs, and internally simulated variables of HBV')
+    # plt.show()
+    fig.savefig(f"C:\\Users\\hfran\\Documents\\Uni\\Master\\hydrology\\MMUQ_Python_Setup\\EclipsePortable426\\Data\\mmuq_ws2425\\hmg\\data\\task_2\\diss_{prm_name}_changed_by_{change_value}_2.png", bbox_inches='tight')
+
+    plt.close(fig)
+
+
+def change_parameter_vector(i, last_prm_value, change_value):
+    last_prm_value.iloc[i] = last_prm_value.iloc[i] * change_value
+
+    # check if new param value is in bounds!
+    # if not set to upper / lower bound
+
+    bound_for_param = bounds_dict[prm_names[i]]
+
+    if last_prm_value.iloc[i] > bound_for_param[1]:
+        last_prm_value.iloc[i] = bound_for_param[1]
+
+    elif last_prm_value.iloc[i] < bound_for_param[0]:
+        last_prm_value.iloc[i] = bound_for_param[0]
+
+    return last_prm_value
+
+
+changed_param_output = []
+
+
+def change_all_params(last_prm_value, change_value, metric):
+    for i in range(0, len(last_prm_value)):
+        print(i)
+        prms = change_parameter_vector(i, last_prm_value, change_value)
+        new_obj_function = objective_function_value(np.array(prms.values, dtype=float), modl_objt, metric, diso)
+
+        otps = modl_objt.get_outputs()
+        diss = modl_objt.get_discharge()
+
+        bound_for_param = bounds_dict[prm_names[i]]
+        output_dict = {
+            "changed_parameter": prm_names[i],
+            'param_changed_by': change_value,
+            'old_param_value': last_prm_value.iloc[i],
+            'new_param_value': prms.iloc[i],
+            'param_bounds': bound_for_param,
+            # TODO belongs to which process?
+            'old_best_obj_fct_value': last_objective_function,
+            'new_obj_fct_value_after_change': new_obj_function,
+            'change_of_obj_fct_new/old': new_obj_function / last_objective_function,
+
+            }
+        print(output_dict)
+        changed_param_output.append(output_dict)
+        plot_output(otps, diss, diso, prm_names[i], change_value)
+
+
+if __name__ == "__main__":
+    modl_objt = HBV1D012A()
+    modl_objt.set_inputs(tems, ppts, pets)
+    modl_objt.set_outputs(tsps)
+    modl_objt.set_discharge_scaler(dslr)
+    otps_lbls = modl_objt.get_output_labels()
+    metric = "nse"
+    change_all_params(last_prm_values, 0.8, metric)  # change here for 1.2 etc.
+
+    df = pd.DataFrame.from_dict(changed_param_output)
+    df.to_csv("C:\\Users\\hfran\\Documents\\Uni\\Master\\hydrology\\MMUQ_Python_Setup\\EclipsePortable426\\Data\\mmuq_ws2425\\hmg\\data\\task_2\\output.csv", index=False)
+
