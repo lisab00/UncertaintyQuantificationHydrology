aac2cd8d80da39369fb3c6151799b0ab3d6a4ea6
hele-135 <74352271+hele-135@users.noreply.github.com>
2025-02-03 13:46:11 +0100

    prm plots

--- task_4/plots_code/task_4_plots_prm_cdf.py
+++ task_4/plots_code/task_4_plots_prm_cdf.py
@@ -1,15 +1,27 @@
 '''
-created on 25.01.25
-@author lisa
-plots for task 4
+Created on 01.02.2025
+
+@author: hfran
 '''
 
-import numpy as np
-import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
-from pathlib import Path
+import numpy as np
+import seaborn as sns
 
+obj_fct_values = pd.read_csv("C:\\Users\\hfran\\Documents\\Uni\\Master\\hydrology\\MMUQ_Python_Setup\\EclipsePortable426\\Data\\mmuq_ws2425\\hmg\\data\\task_1\\output_one_per_iteration_tol_0.01_seed_123.csv", header=None)
+
+obj_fct_values.columns = ["Obj_fct_value", "prm_value"]
+
+prm_val_true = obj_fct_values["prm_value"].iloc[-1]
+prm_val_true = np.array(np.fromstring(prm_val_true.strip('[]'), sep=' '))
+print(len(prm_val_true))
+
+obj_fct_values = pd.read_csv("C:\\Users\\hfran\\Documents\\Uni\\Master\\hydrology\\MMUQ_Python_Setup\\EclipsePortable426\\Data\\mmuq_ws2425\\hmg\\data\\task_4\\input_changes.csv")
+
+obj_fct_values.columns = ["index", "new_ofv_old_params", "prm_value", "new_opt_ofv"]
+df = pd.DataFrame(obj_fct_values['prm_value'])
+print(df.head())
 prm_names = [
         'snw_dth',
         'snw_ast',
@@ -42,33 +54,34 @@ prm_names = [
         'lrr_dro',
 
     ]
+print(len(prm_names))
+print(print(df['prm_value'].iloc[0]))
 
-main_dir = Path(r'C:\Users\lihel\Documents\MMUQ_Python_Setup\MMUQ_Python_Setup\EclipsePortable426\Data\mmuq_ws2425\hmg\UncertaintyQuantificationHydrology')
+df['prm_value'] = df['prm_value'].str.replace("[ ", "")
+df['prm_value'] = df['prm_value'].str.replace("[", "")
+df['prm_value'] = df['prm_value'].str.replace("]", "")
+df['prm_value'] = df['prm_value'].str.split()
+df['prm_value'] = [[float(p) for p in prm] for prm in df['prm_value']]
 
-# import true prm vector from task 1
-iterations_df = pd.read_csv(main_dir / "task_1" / "outputs_task1" / "csv_outputs" / "output_one_per_iteration_tol_0.01_seed_123.csv")
-iterations_df.columns = ["Obj_fct_value", "prm_value"]
-prm_val_true = iterations_df["prm_value"].iloc[-1]
-
-# read input data from task 4
-df_inp_ch = pd.read_csv(main_dir / "task_4" / "output" / "input_changes.csv", index_col=0)
-
-# bring prm_values into correct format
-prm_val_new = np.array([np.fromstring(row.strip('[]'), sep=' ') for row in df_inp_ch["new_opt_params"]]).T.tolist()
-
+df[prm_names] = pd.DataFrame(df['prm_value'].tolist(), index=df.index)
+print(df.head())
+print(len(df[prm_names[0]]))
 # create subplots for each prm
 rows, cols = 5, 5
 fig, axes = plt.subplots(rows, cols, figsize=(20, 15), constrained_layout=True)
+print(len(prm_val_true))
 
-for ax, i in zip(axes.flat, np.arange(len(prm_val_new))):
-    sns.kdeplot(prm_val_new[i], cumulative=True, ax=ax, warn_singular=False)
+for ax, i in zip(axes.flat, range(len(prm_names))):
+    sns.kdeplot(df[prm_names[i]], cumulative=True, ax=ax, warn_singular=False)
     ax.axvline(x=prm_val_true[i], color='orange', linestyle='--', label='True Value')
     ax.set_title(f"{prm_names[i]}")
     ax.set_ylabel("CDF")
-    ax.tick_params(axis='x', labelbottom=False)
+    ax.set_xlabel('')
+    # ax.tick_params(axis='x')
     # ax.legend()
 
-for ax in axes.flat[len(prm_val_new):]:
+for ax in axes.flat[len(prm_val_true):]:
     ax.axis('off')
 
 plt.show()
+
